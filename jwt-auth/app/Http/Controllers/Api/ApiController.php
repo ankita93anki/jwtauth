726f91f6905bda0137460ceadbe058613c0f44ab
Ankita Singh <ankitasingh7639369@gmail.com>
2024-08-26 21:53:47 +0530

    register function

--- jwt-auth/app/Http/Controllers/Api/ApiController.php
+++ jwt-auth/app/Http/Controllers/Api/ApiController.php
@@ -4,13 +4,30 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class ApiController extends Controller
 {
     //Register API - POST (name, email, password)
     public function register(Request $request)
     {
+            $request->validate([
+                "name" => "required|string",
+                "email" => "required|string|email|unique:users",
+                "password" => "required|confirmed"
+            ]);
 
+            //User model to save user in database
+           $data =   User::create([
+                "name" => $request->name,
+                "email" => $request->email,
+                "password" => bcrypt($request->password)
+            ]);
+            return response([
+                "status" => true,
+                "message" => "User created successfully",
+                "data" =>$data
+            ]);
     }
 
     //Login API - POST (email, password)
